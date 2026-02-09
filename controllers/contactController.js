44d19cdcabe2958c9083729b86cc55bd6897fb28
Luu Hiep <luuhiep123mc@gmail.com>
2026-02-10 05:57:03 +0700

    Thêm cấu hình nodemailer vào contactController

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -38,3 +38,13 @@ exports.deleteContact = (req, res) => {
 
   res.json({ msg: "Deleted successfully" })
 }
+
+import nodemailer from "nodemailer"
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+})
